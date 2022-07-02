34538682eb866e205dfe996bf8df015f60f8dac6
Kazuya Takei <myself@attakei.net>
2022-07-03 01:43:51 +0900

    doc: Init env of documentation

---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+# -- Path setup
+
+# -- Project information
+project = "googlefonts-markup"
+copyright = "2022, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0.4.0"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output
+html_theme = "haiku"
+html_static_path = ["_static"]
